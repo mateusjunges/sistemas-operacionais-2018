74d3018c883f5ae49bf82fcfbf97990d680ef537
Mateus Junges <mateusf.junges@gmail.com>
2018-04-25 21:18:31 -0300

    Barbeiro

--- Barbeiro/barbeiro.c
+++ Barbeiro/barbeiro.c
@@ -4,33 +4,34 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define CADEIRAS 5                /* número de cadeiras para os clientes à espera */
+#define CADEIRAS 5                /* nÃºmero de cadeiras para os clientes esperarem */
 #define TRUE 1
 
-sem_t cliente;                  /* número de cliente que estao esperando atendimento */
-sem_t barbeiro;                  /* número de barbeiros à espera de clientes */
-sem_t mutex;                    /* para exclusão mútua */
-int esperando = 0;                /* clientes que estão esperando */
+sem_t sem_clientes;                /* nÃºmero de cliente Ã  espera de atendimento */
+sem_t sem_barbeiros;                  /* nÃºmero de barbeiros Ã  espera de clientes */
+sem_t mutex;                    /* exclusÃ£o mÃºtua */
+int esperando = 0;                /* clientes que estÃ£o esperando */
 
-
-
-void cortando();
-void cliente_chegou();
-void sendoatendido();
-void desistiu();
+/* protÃ³tipos */
+void* barbeiro(void *arg);
+void* cliente(void *arg);
+void cortarCabelo();
+void clienteChegou();
+void cortandoCabelo();
+void clienteDesistiu();
 
 int main() {
-sem_init(&cliente, TRUE, 0);
-sem_init(&barbeiro, TRUE, 0);
+sem_init(&sem_clientes, TRUE, 0);
+sem_init(&sem_barbeiros, TRUE, 0);
 sem_init(&mutex, TRUE, 1);
 
 pthread_t b, c;
 
- /* criando barbeiro */
+ /* criando Ãºnico barbeiro */
  pthread_create(&b, NULL, (void *) barbeiro, NULL);
 
 
- /* criação de clientes */
+ /* criaÃ§Ã£o indefinida de clientes */
  while(TRUE) {
 pthread_create(&c, NULL, (void *) cliente, NULL);
 sleep(1);
@@ -41,48 +42,48 @@ return 0;
 
 void* barbeiro(void *arg) {
 while(TRUE) {
-sem_wait(&cliente);   /* vai dormir se o número de clientes for 0 */
-sem_wait(&mutex);       /* obtém acesso a 'esperando' */
-esperando = esperando - 1;  /*decrementa o contador de clientes à espera */
-sem_post(&barbeiro);     /* um barbeiro está agora pronto para cortar cabelo */
-sem_post(&mutex);       /* libera 'esperando' */
-cortando();             /* corta o cabelo (fora da região crítica) */
+sem_wait(&sem_clientes);   /* vai dormir se o nÃºmero de clientes for 0 */
+sem_wait(&mutex);       /* obtÃ©m acesso a 'waiting' */
+esperando = esperando - 1;  /*descresce de um o contador de clientes Ã  espera */
+sem_post(&sem_barbeiros);     /* um barbeiro estÃ¡ agora pronto para cortar cabelo */
+sem_post(&mutex);       /* libera 'waiting' */
+cortarCabelo();             /* corta o cabelo (fora da regiÃ£o crÃ­tica) */
 }
 
 pthread_exit(NULL);
 }
 
 void* cliente(void *arg) {
-sem_wait(&mutex);           /* entra na região crítica */
+sem_wait(&mutex);           /* entra na regiÃ£o crÃ­tica */
 
-if(esperando < CADEIRAS) {      /* se não houver cadeiras vazias, vá embora */
-cliente_chegou();
-esperando = esperando + 1;  /* incrementa o contador de clientes que estao esperando  */
-sem_post(&cliente);   /* Se precisar, acorda o barbeiro */
-sem_post(&mutex);       /* libera o acesso a 'esperando' */
-sem_wait(&barbeiro);     /* vai dormir se o número de barbeiros livres for 0 */
-sendoatendido();          /* cliente está sentado e sendo atendido pelo barbeiro */
+if(esperando < CADEIRAS) {      /* se nÃ£o houver cadeiras vazias, saia */
+clienteChegou();
+esperando = esperando + 1;  /* incrementa o contador de clientes Ã  espera */
+sem_post(&sem_clientes);   /* acorda o barbeiro se necessÃ¡rio */
+sem_post(&mutex);       /* libera o acesso a 'waiting' */
+sem_wait(&sem_barbeiros);     /* vai dormir se o nÃºmero de barbeiros livres for 0 */
+cortandoCabelo();          /* sentado e sendo servido */
 } else {
-sem_post(&mutex);       /* a barbearia está cheia, o cliente não espera e vai embora */
-desistiu();
+sem_post(&mutex);       /* a barbearia estÃ¡ cheia; nÃ£o espera */
+clienteDesistiu();
 
 }
 
 pthread_exit(NULL);
 }
 
-void cortando() {
-printf("Barbeiro cortando cabelo de um cliente!\n");
+void cortarCabelo() {
+printf("Barbeiro esta cortando o cabelo de alguem!\n");
 sleep(3);
 }
 
-void cliente_chegou() {
-printf("Um cliente chegou\n");
+void clienteChegou() {
+printf("Cliente chegou para cortar cabelo!\n");
 }
-void sendoatendido() {
-printf("Cliente sendo atendido\n");
+void cortandoCabelo() {
+printf("Cliente esta sendo atendido!\n");
 }
 
-void desistiu() {
-printf("O cliente foi embora porque a barbearia esta cheia\n");
+void clienteDesistiu() {
+printf("Cliente desistiu!\n");
 }
