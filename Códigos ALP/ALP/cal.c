35eeee80d339ad4241039c69cb734e8e8f7297b0
Mateus Junges <mateusf.junges@gmail.com>
2018-06-12 09:20:21 -0300

    Finalizado calendário

--- Códigos ALP/ALP/cal.c
+++ Códigos ALP/ALP/cal.c
@@ -12,9 +12,9 @@ extern char* params;
 static char* page_start = //conteudo a ser exibido no inicio da pagina
         "<html>\n"
         "<head>\n"
-        "    <meta charset=\"utf-8\">\n"
-	"<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.0.13/css/all.css' integrity='sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp' crossorigin='anonymous'>"
-        "</head>\n"
+            "<title>Calendário - SO</title>"
+            "<meta charset=\"utf-8\">\n"
+	    "</head>\n"
        " <body>\n"
 	"<div class='inicio'>"
 		"<h1>Módulo de calendário - ALP - SO 2018</h1>\n"
@@ -34,7 +34,7 @@ static char* page_end = //conteudo html a ser exibido no fim da pagina
 	" bottom: 0;"
 	"width: 100%;"
 	"color: black;"
-	"background: white;"	
+	"background: white;"
 	"text-align: center;"
 	"text-decoration: none;"
 "}"
@@ -73,7 +73,7 @@ static char* page_end = //conteudo html a ser exibido no fim da pagina
 		"<h4><a href='https://mateusjunges.com'"
 			"style='text-decoration: none; color: #000000;'"
 			"class=''>Mateus Junges</a></h4>"
-		"<a href='mailto:contato@mateusjunges.com'><i class='fa fa-at'></i></a>"
+		"<a href='mailto:contato@mateusjunges.com'>Email</a>"
  		"</div>"
 	"</div> "
 "</body>\n"
@@ -83,7 +83,7 @@ static char* page_end = //conteudo html a ser exibido no fim da pagina
 "</html>\n";
 
 
-static char* erro = 
+static char* erro =
 		"<div style='background: red;\n"
 		     " margin: auto;\n"
 		     "border-radius: 10%;\n"
@@ -136,7 +136,7 @@ void module_generate(int fd) {
         rval = dup2(fd, STDERR_FILENO);
         if (rval == -1)
             system_error("dup2");
- 
+
         if (mes == NULL) { //se nao passou o parametro para o mes
             if (flag == 1) {
                 write(fd, "<div style='background: red;\n"
@@ -153,33 +153,33 @@ void module_generate(int fd) {
 		     "<h1>Os parâmetros corretos são ano=ano&mes=mes</h1>\n"
                "</div>\n"));
             }
-            if (ano == NULL) { // se o ano for Nulo, executa o comando cal com o parametro de help
-                char* argv[] = {"/usr/bin/cal", "-h", NULL};
+            if (ano == NULL) { // se o ano for NULL e mes for NULL
+                char* argv[] = {"/usr/bin/cal", NULL, NULL}; //Se passar só o cal na URL, busca o mes corrente do ano corrente
                 execv(argv[0], argv);
-            } else {
+            } else { // Se o mes é NULL e o ano existe
                 if (a < 0 || a > 9999 || a == NULL) { //se for um ano não válido
                     write(fd, "<div style='background: red; text-align: center;'><h1><b>Parâmetros incorretos</b></h1>\n", strlen("<div style='background: red; text-align: center;'><h1><b>Parâmetros incorretos</b></h1>\n"));
-                write(fd, "<br><h3><b>Ano deve ser um número entre 0 e 9999</b></h3>\n", strlen("<br><h3><b>Mês deve ser um número entre 1 e 12</b></h3></div>\n"));
-                    char* argv[] = {"/usr/bin/cal", "-h", NULL};
+                    write(fd, "<br><h3><b>Ano deve ser um número entre 0 e 9999</b></h3>\n", strlen("<br><h3><b>Mês deve ser um número entre 1 e 12</b></h3></div>\n"));
+                    char* argv[] = {"/usr/bin/cal", "-h", NULL}; //Executa o cal com o parametro '-h'
                     execv(argv[0], argv);
                 } else {
                     char* argv[] = {"/usr/bin/cal", ano, NULL};
                     execv(argv[0], argv);
                 }
             }
-        } else {
+        } else { //se o mes não for NULL
             if (m < 1 || m > 12 || m == NULL) { // se for um mes não válido
                 write(fd, "<div style='background: red; text-align: center;'><h1><b>Parâmetros incorretos</b></h1>\n", strlen("<div style='background: red; text-align: center;'><h1><b>Parâmetros incorretos</b></h1>\n"));
                 write(fd, "<br><h3><b>Mês deve ser um número entre 1 e 12</b></h3>\n", strlen("<br><h3><b>Mês deve ser um número entre 1 e 12</b></h3></div>\n"));
                 char* argv[] = {"/usr/bin/cal", "-h", NULL};
                 execv(argv[0], argv);
             } else {
-                if (ano == NULL) {
+                if (ano == NULL) {//se for um mês válido mas o ano está NULL
                     if (flag == 1) {
                         write(fd, "<div style='background: red; margin: auto; text-align: center; width: 80%; border-radius: 10%'><h1>Os parâmetros corretos são: ano=ano&mes=mes</h1></div>\n\n", strlen("<div style='background:red; margin: auto; text-align: center; width: 80%; border-radius: 10%'><h1>Os parâmetros corretos são ano=ano&mes=mes</h1></div>\n"));
                     }
                     char* argv[] = {"/usr/bin/cal", "-m", mes, NULL};
-                    execv(argv[0], argv);
+                    execv(argv[0], argv); //Executa o comando cal com o parametro -m e o mes passado na URL
                 } else {
                     if (a < 0 || a > 9999 || a == NULL) { //se o ano existe mas não é valido
                         write(fd, "<div style='background: red;'><h1 style='text-align: center;><b>Formato incorreto</b></h1>\n", strlen("<div style='background: red;'><h1 style='text-align: center;><b>Formato incorreto</b></h1>\n"));
@@ -187,11 +187,11 @@ void module_generate(int fd) {
                         char* argv[] = {"/usr/bin/cal", "-h", NULL};  //executa cal com -h
                         execv(argv[0], argv);
                     } else {
-			/* Se for ano e mes valido executa o 
-			cal com os parametros ano e mes 
-			passados na URL */
+                                /* Se for ano e mes valido executa o
+                                cal com os parametros ano e mes
+                                passados na URL */
                         char* argv[] = {"/usr/bin/cal", mes, ano, NULL};
-                        execv(argv[0], argv); 
+                        execv(argv[0], argv); //Executa o cal com o ano e o mes passados na URL
                     }
                 }
             }
@@ -202,7 +202,7 @@ void module_generate(int fd) {
         rval = waitpid(child_pid, NULL, 0);
         if (rval == -1)
             system_error("waitpid"); //se erro
-    } else	
+    } else
         system_error("fork");
     write(fd, page_end, strlen(page_end)); //final da pagina
 }
