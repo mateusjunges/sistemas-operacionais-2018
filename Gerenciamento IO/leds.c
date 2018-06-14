3c9723469a4cb2a1da65ea07bc6c13f5268f0344
Mateus Junges <mateusf.junges@gmail.com>
2018-06-13 21:30:35 -0300

    leds

--- Gerenciamento IO/leds.c
+++ Gerenciamento IO/leds.c
@@ -4,9 +4,46 @@
 
 // Pesquisar sobre: 
 //	- ioperm()
+	/*
+	ioperm()  sets  the  port access permission bits for the calling thread
+       for num bits starting from port address from.  If turn_on  is  nonzero,
+       then permission for the specified bits is enabled; otherwise it is dis‐
+       abled.  If turn_on is nonzero, the calling thread  must  be  privileged
+       (CAP_SYS_RAWIO).
+	*/
 //	- setuid()
+	/*setuid()  sets  the  effective  user ID of the calling process.  If the
+       calling process is privileged (more precisely: if the process  has  the
+       CAP_SETUID  capability  in  its user namespace), the real UID and saved
+       set-user-ID are also set.
+
+       Under Linux, setuid() is implemented like the POSIX  version  with  the
+       _POSIX_SAVED_IDS  feature.  This allows a set-user-ID (other than root)
+       program to drop all of its user privileges, do some un-privileged work,
+       and then reengage the original effective user ID in a secure manner.
+
+	*/
+
 //	- outb()
+	/*
+	 This family of functions is used to do low-level port input and output.
+       The out* functions do port output, the in* functions do port input; the
+       b-suffix functions are byte-width  and  the  w-suffix  functions  word-
+       width; the _p-suffix functions pause until the I/O completes.
+
+       They  are  primarily  designed for internal kernel use, but can be used
+       from user space.
+	*/
+
 //	- inb()
+	/* This family of functions is used to do low-level port input and output.
+       The out* functions do port output, the in* functions do port input; the
+       b-suffix functions are byte-width  and  the  w-suffix  functions  word-
+       width; the _p-suffix functions pause until the I/O completes.
+
+       They  are  primarily  designed for internal kernel use, but can be used
+       from user space.
+	*/
 //	- controlador do teclado: 
 //		- http://www.brokenthorn.com/Resources/OSDev19.html
 
